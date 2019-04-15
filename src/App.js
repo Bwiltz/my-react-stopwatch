c4fdd114aec845f0a45e2b1c8d1d4ee4f39a7799
Brian Wiltz <bwiltz@scitent.com>
2019-04-15 01:07:05 -0400

    updates app.js and adds ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
+// src/App.js
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Stopwatch</h1>
+        <Stopwatch />
       </div>
     );
   }
+}
+
+function Stopwatch() {
+  return (
+    <div>
+      <p>0ms</p>
+      <button>Start</button>
+      <button>Reset</button>
+    </div>
+  );
 }
 
 export default App;
